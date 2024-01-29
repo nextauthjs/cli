de692633789a43dfa7ba87587ba3702fd528573d
Nico Domino <yo@ndo.dev>
2024-01-29 03:22:01 +0100

    feat: add clipboard helper (#2)
    * feat: add clipboard helper
    
    * feat: add chalk devDep
    
    * fix: restructure command actions into their own files
    
    * update
    
    * add ts-check
    
    ---------
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

---
+++ commands/secret.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+import * as y from "yoctocolors"
+import { write } from "../lib/clipboard/index.js"
+
+/** Web compatible method to create a random string of a given length */
+function randomString(size = 32) {
+  const bytes = crypto.getRandomValues(new Uint8Array(size))
+  // @ts-expect-error
+  return Buffer.from(bytes, "base64").toString("base64")
+}
+
+export async function action(options) {
+  let value = randomString()
+  if (options.raw) return console.log(value)
+
+  value = `AUTH_SECRET="${value}"`
+  if (!options.copy) return console.log(value)
+
+  const message = {
+    introClipboard:
+      "Secret generated. It has been copied to your clipboard, paste it to your .env/.env.local file to continue.",
+    introCopy:
+      "Secret generated. Copy it to your .env/.env.local file (depending on your framework):",
+    value: `\n${value}`,
+  }
+
+  // TODO: Detect framework, check for existing value, and write automatically
+  try {
+    write(value)
+    console.log(message.introClipboard)
+  } catch (error) {
+    console.error(y.red(error))
+    console.log(message.introCopy)
+  } finally {
+    console.log(message.value)
+    process.exit(0)
+  }
+}
--- index.js
+++ index.js
@@ -1,60 +1,31 @@
 #!/usr/bin/env node
 
+// @ts-check
+
 import { InvalidArgumentError } from "commander"
 import { Command } from "commander"
+import { frameworks, action as frameworkAction } from "./commands/framework.js"
+import { action as secretAction } from "./commands/secret.js"
 // import pkg from "./package.json" assert { type: "json" }
 
 import fs from "fs/promises"
 import { join } from "path"
 import { fileURLToPath } from "url"
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
-const pkg = JSON.parse(await fs.readFile(join(__dirname, "./package.json")))
+const pkg = JSON.parse(
+  await fs.readFile(join(__dirname, "./package.json"), "utf-8")
+)
 const { name, description, version } = pkg
 
 try {
   // TODO: Remove when Node.js 18 is not maintained anymore
+  // @ts-expect-error
   globalThis.crypto ??= (await import("crypto")).webcrypto
 } catch {}
-
-/** Web compatible method to create a random string of a given length */
-export function randomString(size = 32) {
-  const bytes = crypto.getRandomValues(new Uint8Array(size))
-  return Buffer.from(bytes, "base64").toString("base64")
-}
 
 const program = new Command()
 
 program.name(name).description(description).version(version)
-
-program
-  .command("secret")
-  .option("--raw", "Output the string without any formatting.")
-  .description("Generate a random string.")
-  .action((options) => {
-    const value = randomString()
-    if (options.raw) return console.log(value)
-    // TODO: Detect framework, check for existing value, and write automatically
-    console.log(`
-Secret generated. Copy it to your .env/.env.local file (depending on your framework):
-
-AUTH_SECRET=${value}`)
-  })
-
-// TODO: Get this programmatically
-const frameworks = {
-  nextjs: {
-    src: "https://github.com/nextauthjs/next-auth-example",
-    demo: "https://next-auth-example.vercel.app",
-  },
-  sveltekit: {
-    src: "https://github.com/nextauthjs/sveltekit-auth-example",
-    demo: "https://sveltekit-auth-example.vercel.app",
-  },
-  express: {
-    src: "https://github.com/nextauthjs/express-auth-example",
-    demo: "https://express-auth-example.vercel.app",
-  },
-}
 
 program
   .command("framework")
@@ -62,19 +33,19 @@ program
     if (!value) return value
     if (Object.keys(frameworks).includes(value)) return value
     throw new InvalidArgumentError(
-      `Valid frameworks are: ${supportedFrameworks.join(", ")}`
+      `Valid frameworks are: ${frameworks.join(", ")}`
     )
   })
   .description("Clone a framework template.")
-  .action((framework) => {
-    if (!framework) {
-      return console.log(`
-Supported frameworks are: ${Object.keys(frameworks).join(", ")}`)
-    }
-    const { src, demo } = frameworks[framework]
-    console.log(`
-Source code: ${src}
-Deployed demo: ${demo}`)
-  })
+  .action(frameworkAction)
 
-program.parse(process.argv)
+program
+  .command("secret")
+  .option("--raw", "Output the string without any formatting.")
+  .option("--copy", 'Copy AUTH_SECRET="value"')
+  .description("Generate a random string.")
+  .action(secretAction)
+
+program.parse()
+
+export { program }
